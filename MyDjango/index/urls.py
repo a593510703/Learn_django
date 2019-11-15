374e90eefe23c3da44b2297416edca0e46b40e1c
ksyer <593510703@qq.com>
2019-11-15 18:03:49 +0800

    第四章结束

--- MyDjango/index/urls.py
+++ MyDjango/index/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('login.html', views.login),
-    
+    path('index/', views.ProductList.as_view()),
+    path('index/<id>.html', views.ProductList.as_view(), {'name': 'phone'})
 ]
--- MyDjango/index/views.py
+++ MyDjango/index/views.py
@@ -2,14 +2,22 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import csv
 from .models import Product
+from django.views.generic import ListView
 
-def login(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        return redirect('/')
-    else:
-        if request.GET.get('name'):
-            name = request.GET.get('name')
-        else:
-            name = 'Everyone'
-        return HttpResponse('username is ' + name)
+class ProductList(ListView):
+    context_object_name = 'type_list'
+    template_name = 'index_view.html'
+    queryset = Product.objects.values('type').distinct()
+
+    def get_queryset(self):
+        print(self.kwargs['id'])
+        print(self.kwargs['name'])
+        print(self.request.method)
+        type_list = Product.objects.values('type').distinct()
+        return type_list
+        
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name_list'] = Product.objects.values('name', 'type')
+        return context
