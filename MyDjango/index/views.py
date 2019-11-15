aee94c130ba92e656ba39f0fbcf861d4bb6db5a7
ksyer <593510703@qq.com>
2019-11-15 10:37:50 +0800

    第三章结束

---
+++ MyDjango/index/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello world!")
+
+def mydate(request, year, month, day):
+    return HttpResponse("%s/%s/%s" % (str(year), str(month), str(day)))
+
+def myyear(request, year):
+    return render(request, 'myyear.html')
+
+def myyear_dict(request, year, month):
+    return render(request, 'myyear_dict.html', {'month': month})
